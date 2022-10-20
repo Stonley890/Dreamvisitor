9cd6ce8952f6666f14980e414fa091581af9ccb3
Stonley890 <79172597+Stonley890@users.noreply.github.com>
2022-10-19 17:17:49 -0700

    Pause chat

--- dreamvisitor/src/main/java/io/github/stonley890/App.java
+++ dreamvisitor/src/main/java/io/github/stonley890/App.java
@@ -1,51 +1,38 @@
 package io.github.stonley890;
 
-//import java.util.UUID;
-//import java.util.logging.Logger;
-
 import javax.security.auth.login.LoginException;
 
-//import org.bukkit.OfflinePlayer;
-//import org.bukkit.command.Command;
-//import org.bukkit.command.CommandSender;
-//import org.bukkit.entity.Player;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-//import org.bukkit.inventory.InventoryView;
 import org.bukkit.plugin.java.JavaPlugin;
-//import org.jetbrains.annotations.NotNull;
-//import org.jetbrains.annotations.Nullable;
-
-//import com.lishid.openinv.IOpenInv;
-//import com.lishid.openinv.internal.IAnySilentContainer;
-//import com.lishid.openinv.internal.ISpecialEnderChest;
-//import com.lishid.openinv.internal.ISpecialInventory;
-//import com.lishid.openinv.internal.ISpecialPlayerInventory;
 
 import io.github.stonley890.commands.CommandsManager;
 import net.dv8tion.jda.api.entities.Guild;
-//import net.dv8tion.jda.api.entities.TextChannel;
 
 public class App extends JavaPlugin implements Listener {
 
-    String token = "BOT_TOKEN";
     private static App plugin;
+    private static boolean chatPaused;
 
     @Override
     public void onEnable() {
 
         getLogger()
-                .info("Dreamvisitor: A plugin created by Bog for WoF:TNW to add Discord automation.");
+                .info("Dreamvisitor: A plugin created by Bog for WoF:TNW to add various features.");
         getServer().getPluginManager().registerEvents(this, this);
         // Start Discord bot
         try {
             new Bot();
         } catch (LoginException e) {
-            System.out.println("ERROR: Bot login failed!");
+            Bukkit.getLogger().warning("ERROR: Bot login failed!");
             e.printStackTrace();
         }
         try {
@@ -53,7 +40,8 @@ public class App extends JavaPlugin implements Listener {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        Bot.getJDA().getGuilds().forEach((Guild guild) -> guild.getSystemChannel().sendMessage("Server has been started.").queue());
+        Bot.getJDA().getGuilds()
+                .forEach((Guild guild) -> guild.getSystemChannel().sendMessage("Server has been started.").queue());
 
         plugin = this;
 
@@ -63,36 +51,34 @@ public class App extends JavaPlugin implements Listener {
         return plugin;
     }
 
-//    @EventHandler
-//    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-//        if (command.getName() == "offlineinv") {
-//            if (sender instanceof Player) {
-//                Player player = (Player) sender;
-//                try {
-//                    IOpenInv openinv = new IOpenInv() {
-//                    };
-//                    openinv.getSpecialEnderChest(player, true);
-//                } catch (InstantiationException e) {
-//                    e.printStackTrace();
-//                }
-//                return true;
-//            } else {
-//                return false;
-//            }
-//        } else {
-//            return false;
-//        }
-//    }
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (label.equalsIgnoreCase("pausechat")) {
+            if (chatPaused == true) {
+                chatPaused = false;
+                Bukkit.getServer().broadcastMessage(ChatColor.GOLD + "Chat has been unpaused.");
+            } else {
+                chatPaused = true;
+                Bukkit.getServer().broadcastMessage(ChatColor.GOLD + "Chat has been paused.");
+            }
+
+        }
+        return true;
+    }
 
     @EventHandler
     public void onPlayerChatEvent(AsyncPlayerChatEvent event) {
         // Send chat messages to Discord
-        String chatMessage = "**" + event.getPlayer().getName() + "**: " + event.getMessage();
-        String channelId = CommandsManager.getChatChannel();
-        if (channelId != "none") {
-            io.github.stonley890.Bot.getJDA().getTextChannelById(channelId).sendMessage(chatMessage).queue();
+        if (chatPaused != true && event.getPlayer().isOp() == false || event.getPlayer().isOp() == true) {
+            String chatMessage = "**" + event.getPlayer().getName() + "**: " + event.getMessage();
+            String channelId = CommandsManager.getChatChannel();
+            if (channelId != "none") {
+                io.github.stonley890.Bot.getJDA().getTextChannelById(channelId).sendMessage(chatMessage).queue();
+            }
+        } else {
+            event.setCancelled(true);
+            event.getPlayer().sendMessage(ChatColor.RED + "Chat is currently paused.");
         }
-        
 
     }
 
@@ -129,10 +115,11 @@ public class App extends JavaPlugin implements Listener {
     @Override
     public void onDisable() {
         getLogger().info("Dreamvisitor disable process.");
-        Bot.getJDA().getGuilds().forEach((Guild guild) -> guild.getSystemChannel().sendMessage("Server has been shutdown.").queue());
+        Bot.getJDA().getGuilds()
+                .forEach((Guild guild) -> guild.getSystemChannel().sendMessage("Server has been shutdown.").queue());
         // Shut down bot
         Bot.getJDA().shutdown();
-        
+
     }
 
 }
