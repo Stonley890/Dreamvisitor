8e73adc00a2fbb2778e8165e6abd7d1d53f6e269
Stonley890 <79172597+Stonley890@users.noreply.github.com>
2024-02-18 15:52:45 -0800

    forgot these

---
+++ dreamvisitor/src/main/java/io/github/stonley890/dreamvisitor/commands/CmdItemBanList.java
@@ -0,0 +1,27 @@
+package io.github.stonley890.dreamvisitor.commands;
+
+import io.github.stonley890.dreamvisitor.functions.ItemBanList;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class CmdItemBanList implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+
+        if (sender instanceof Player player) {
+
+            if (ItemBanList.badItems != null) {
+                ItemBanList.inv.setContents(ItemBanList.badItems);
+            }
+
+            player.openInventory(ItemBanList.inv);
+        }
+
+        return true;
+    }
+
+}
---
+++ dreamvisitor/src/main/java/io/github/stonley890/dreamvisitor/functions/ItemBanList.java
@@ -0,0 +1,48 @@
+package io.github.stonley890.dreamvisitor.functions;
+
+import io.github.stonley890.dreamvisitor.Bot;
+import io.github.stonley890.dreamvisitor.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+public class ItemBanList implements Listener {
+    public static final Inventory inv = Bukkit.createInventory(null, 27, "Blacklisted Items");
+    public static ItemStack[] badItems;
+
+    public static void saveItems() {
+        Main plugin = Main.getPlugin();
+        badItems = inv.getContents();
+        plugin.getConfig().set("itemBlacklist", badItems);
+        plugin.saveConfig();
+    }
+
+    @EventHandler
+    public void onInventoryClose(@NotNull InventoryCloseEvent event) {
+        Player player = (Player) event.getPlayer();
+
+        if (!player.isOp() && ItemBanList.badItems != null) {
+
+            for (ItemStack item : ItemBanList.badItems) {
+                if (item == null) continue;
+                for (ItemStack content : player.getInventory().getContents()) {
+                    if (content == null || !content.isSimilar(item)) continue;
+                    player.getInventory().remove(item);
+                    Bot.sendMessage(Bot.gameLogChannel, "Removed " + item.getType().name() + " (" + Objects.requireNonNull(item.getItemMeta()).getDisplayName() + ") from " + player.getName());
+                }
+            }
+        }
+
+        if (event.getInventory().equals(ItemBanList.inv)) {
+            ItemBanList.saveItems();
+        }
+    }
+
+}
